54c3648ffc5b4c70d833491aaa0e9d350717897b
EsotericZ <cjsand03@gmail.com>
2022-01-09 12:02:39 -0700

    created server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const dbData = require('./db/db.json');
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(dbData));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
