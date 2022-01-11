b19359ed6dd85e5ea07717045b7d3937983a0f85
EsotericZ <cjsand03@gmail.com>
2022-01-10 19:51:24 -0700

    fixed /notes page send to

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const dbData = require('./db/db.json');
 const PORT = 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -12,7 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.redirect('/notes.html');
+    // res.redirect('notes.html');
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json(dbData));
